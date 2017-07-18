970b1aa3979db6c930f3badf72938d148956434c
CodeOpsTechnologies <reachus@codeops.tech>
2017-07-18 11:40:42 +0530

    Update get_pages.py

--- get_pages.py
+++ get_pages.py
@@ -11,11 +11,46 @@ def elicit_intent(message):
 		}
 	}
 
+def elicit(message):
+	return {
+		'dialogAction' : {
+		'type' : 'ElicitIntent',
+		'message' : message,
+		'responseCard' : {
+			'version' : 1,
+			'contentType' : 'application/vnd.amazonaws.card.generic',
+			'genericAttachments' : [
+			{
+				'title' : 'These are the trending technologies (as of TIOBE index)',
+				'buttons' : [
+				{
+					'text' : 'Java',
+					'value' : 'show java book'
+				},
+				{
+					'text' : 'C',
+					'value' : 'show C book'
+				},
+				{
+					'text' : 'Python',
+					'value' : 'show python book'
+				}
+			]
+		}
+		]
+	}
+		}
+	}
+
 def lambda_handler(event, context):
     print event
-    session_attributes = event['sessionAttributes']['data']
-    decoded = base64.b64decode(session_attributes)
-    print 'decoded', decoded
-    data = json.loads(decoded)
-    message = {'contentType': 'PlainText', 'content': """The book {} has {} pages""".format(data[0]['title'], data[0]['pages'])}
-    return elicit_intent(message)
+    try:
+        session_attributes = event['sessionAttributes']['data']
+        decoded = base64.b64decode(session_attributes)
+        print 'decoded', decoded
+        data = json.loads(decoded)
+        message = {'contentType': 'PlainText', 'content': """The book {} has {} pages""".format(data[0]['title'], data[0]['pages'])}
+        return elicit_intent(message)
+    except TypeError:
+        message = {'contentType': 'PlainText', 'content': """:-O Oops I forgot what we were talking about. How about these?"""}
+        return elicit(message)
